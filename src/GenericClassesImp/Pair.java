5c406b09dde77fbcbaf6aca45662b15262dec464
Manish Y <manish.kumar@yeppersindia.com>
2025-03-22 15:20:01 +0530

    Generic Class

---
+++ src/GenericClassesImp/Pair.java
@@ -0,0 +1,20 @@
+package GenericClassesImp;
+
+// we are creating more than one type of generic class
+public class Pair<T,K> {
+    T key;
+    K value;
+    public void put(T key, K value){
+        this.key=key;
+        this.value=value;
+    }
+}
+/*
+public class Main{
+    public static void main(String args[]){
+        Pair<Integer, String> p = new Pair<>();
+        p.put(1,"one");
+    }
+}
+
+ */
